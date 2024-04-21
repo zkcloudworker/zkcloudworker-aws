c9881659fca7d72b864d16e839ec754124edbce4
DFST <mk@dfst.io>
2024-04-21 12:37:37 +0300

    restart validation

--- src/table/kv.ts
+++ src/table/kv.ts
@@ -1,4 +1,33 @@
 import { Table } from "./table";
 import { KeyValueData } from "../model/kvData";
+const KV_TABLE = process.env.KV_TABLE!;
 
 export class KeyValue extends Table<KeyValueData> {}
+
+export async function getSystemDataByKey(
+  key: string
+): Promise<string | undefined> {
+  const kvTable = new KeyValue(KV_TABLE);
+  const result = await kvTable.get({
+    repoId: "system",
+    keyId: key,
+  });
+  return result?.valueJSON;
+}
+
+export async function saveSystemDataByKey(
+  key: string,
+  data: string
+): Promise<void> {
+  const kvTable = new KeyValue(KV_TABLE);
+  try {
+    await kvTable.create({
+      repoId: "system",
+      keyId: key,
+      valueJSON: data,
+    });
+  } catch (error) {
+    console.error("saveDataByKey error: ", error);
+    return undefined;
+  }
+}
