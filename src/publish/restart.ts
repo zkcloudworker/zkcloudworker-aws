1effa83985d55fda5fcf73a052ec99a2e1f39423
DFST <mk@dfst.io>
2024-06-22 22:39:35 +0400

    restart nats

--- src/publish/restart.ts
+++ src/publish/restart.ts
@@ -1,45 +1,73 @@
 import {
   LightsailClient,
-  GetInstanceStateCommand,
+  //GetInstanceStateCommand,
   RebootInstanceCommand,
   GetInstanceMetricDataCommand,
   GetInstanceMetricDataCommandInput,
 } from "@aws-sdk/client-lightsail";
+import { sleep } from "../cloud";
 
 export async function restartNatsServer() {
   const client = new LightsailClient();
+  /*
   const params = {
     instanceName: "Celestia",
   };
   const command = new GetInstanceStateCommand(params);
   const data = await client.send(command);
+  //console.log("Instance state:", data);
   const running = data.state?.name === "running";
+  */
   const time = Date.now();
   const metricParams = {
     instanceName: "Celestia",
     metricName: "StatusCheckFailed",
     period: 60,
-    startTime: new Date(time - 60 * 1000),
+    startTime: new Date(time - 240 * 1000),
     endTime: new Date(time),
     unit: "Count",
     statistics: ["Sum"],
   } as GetInstanceMetricDataCommandInput;
   const metric = new GetInstanceMetricDataCommand(metricParams);
-  const metricOutput = await client.send(metric);
-  //console.log("Metric data:", metricOutput);
-  const sum = metricOutput.metricData
-    ? metricOutput.metricData[0]?.sum ?? 0
-    : 0;
+  let count = 0;
+  while (count < 5) {
+    const metricOutput = await client.send(metric);
+    if (count > 0) {
+      console.log("Metric output:", {
+        metricOutput,
+        metricData: metricOutput.metricData,
+      });
+    }
+    if (
+      metricOutput.metricData === undefined ||
+      metricOutput.metricData[0]?.sum === undefined
+    ) {
+      console.error("Metric data is undefined", {
+        metricOutput,
+        metricData: metricOutput.metricData,
+      });
+      await sleep(1000);
+      count++;
+      continue;
+    }
+    //console.log("Metric data:", metricOutput);
+    const sum = metricOutput.metricData
+      ? metricOutput.metricData[0]?.sum ?? 0
+      : 0;
 
-  if (running === false || sum > 0) {
-    console.error("Rebooting the Celestia instance", {
-      running,
-      sum,
-      metricOutput,
-      state: data.state,
-    });
-    const reboot = new RebootInstanceCommand(params);
-    const result = await client.send(reboot);
-    console.log("Reboot result:", result);
+    //console.log("Sum:", sum);
+    if (sum > 0) {
+      console.error("Rebooting the Celestia instance", {
+        sum,
+        metricData: metricOutput.metricData,
+      });
+      const rebootParams = {
+        instanceName: "Celestia",
+      };
+      const reboot = new RebootInstanceCommand(rebootParams);
+      const result = await client.send(reboot);
+      console.log("Reboot result:", result);
+    }
+    return;
   }
 }
